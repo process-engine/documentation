4b586bafb492a9293072df1a4284d4411a9f94fc
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-08 11:58:28 +0100

    :sparkles: add sucidal prod-build-script

---
+++ build_prod.sh
@@ -0,0 +1,7 @@
+npm run render_api_doc
+mv -f ./public/index.html ./andere-themen/cheat-sheet/http-rest-api/apidoc.html
+./node_modules/.bin/gitbook build
+mkdir cache
+mv ./* ./cache/
+mv ./cache/_book/* ./
+rm -rf cache
