e63724b9a9e1d3d22061d69310897689c4d917b2
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-12 16:03:16 +0100

    :construction: use real temp folder for caching

--- build_prod.sh
+++ build_prod.sh
@@ -2,7 +2,7 @@ npm run render_api_doc
 mv -f ./public/index.html ./andere-themen/cheat-sheet/http-rest-api/apidoc.html
 ./node_modules/.bin/gitbook build
 
-mkdir cache
-mv ./!(cache) ./cache/
-mv ./cache/_book/* ./
-rm -rf cache
+TEMP_DIR=`mktemp -d`
+mv ./* $TEMP_DIR/
+mv $TEMP_DIR/_book/* ./
+rm -rf $TEMP_DIR
