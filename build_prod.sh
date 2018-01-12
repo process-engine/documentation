cd50d5b4e60aad0d637d6bd89b99250767d18e72
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-12 18:13:43 +0100

    :bug: set merge strategy

--- build_prod.sh
+++ build_prod.sh
@@ -2,7 +2,7 @@ git checkout develop
 git pull
 git checkout gh-pages
 git pull
-git merge -X theirs develop
+git merge -s theirs develop
 npm install
 npm run render_api_doc
 mv -f ./public/index.html ./andere-themen/cheat-sheet/http-rest-api/apidoc.html
