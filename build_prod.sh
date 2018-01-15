301fcbd1bb59bdc444432a279050d184df59d9e7
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-15 08:47:22 +0100

    :memo: add explaining comments to build-script

--- build_prod.sh
+++ build_prod.sh
@@ -1,3 +1,4 @@
+# make the gh-pages-branch be identical to develop
 git checkout gh-pages
 git pull
 git checkout develop
@@ -6,11 +7,17 @@ git merge --strategy=ours gh-pages
 git checkout gh-pages
 git merge develop
 
+# install the necessary dependencies to build the gitbook
 npm install
+
+# create the api-docs from the swagger.json and move it to its destination
 npm run render_api_doc
 mv -f ./public/index.html ./andere-themen/cheat-sheet/http-rest-api/apidoc.html
+
+# build the gitbook
 ./node_modules/.bin/gitbook build
 
+# make the root contain only the built gitbook and nothing else
 TEMP_DIR=`mktemp -d`
 mv ./* $TEMP_DIR/
 mv $TEMP_DIR/_book/* ./
